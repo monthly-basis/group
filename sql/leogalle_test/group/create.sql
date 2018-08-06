6f7bdb2097b3c6a01f353a1b7142729ccedca8fb
Leo Galleguillos <leo.galleguillos@icloud.com>
2018-08-06 21:14:59 +0000

    Add unique key on `name` column

--- sql/leogalle_test/group/create.sql
+++ sql/leogalle_test/group/create.sql
@@ -1,5 +1,6 @@
 CREATE TABLE `group` (
   `group_id` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `name` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  PRIMARY KEY (`group_id`)
+  PRIMARY KEY (`group_id`),
+  UNIQUE `name` (`name`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
