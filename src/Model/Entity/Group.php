96196b10dc01f05d81d9285579dcf67af319a23f
Leo Galleguillos <leo.galleguillos@icloud.com>
2018-08-06 15:41:19 +0000

    Write group entity class

---
+++ src/Model/Entity/Group.php
@@ -0,0 +1,32 @@
+<?php
+namespace LeoGalleguillos\Group\Model\Entity;
+
+use LeoGalleguillos\Group\Model\Entity as GroupEntity
+
+class Group
+{
+    protected $groupId;
+    protected $name;
+
+    public function getGroupId() : int
+    {
+        return $this->groupId;
+    }
+
+    public function getName() : string
+    {
+        return $this->name;
+    }
+
+    public function setGroupId(int $groupId) : GroupEntity\Group
+    {
+        $this->groupId = $groupId;
+        return $this;
+    }
+
+    public function setName(string $name) : GroupEntity\Group
+    {
+        $this->name = $name;
+        return $this;
+    }
+}
