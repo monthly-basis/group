84bc188b368e14cc5fb0a1e1077801a4c196e780
Leo Galleguillos <leo.galleguillos@icloud.com>
2020-05-02 13:44:49 +0000

    Update table unit tests

--- test/Model/Table/GroupUserTest.php
+++ test/Model/Table/GroupUserTest.php
@@ -3,7 +3,7 @@ namespace LeoGalleguillos\GroupTest\Model\Table;
 
 use Generator;
 use LeoGalleguillos\Group\Model\Table as GroupTable;
-use LeoGalleguillos\GroupTest\TableTestCase;
+use LeoGalleguillos\Test\TableTestCase;
 use Zend\Db\Adapter\Adapter;
 use PHPUnit\Framework\TestCase;
 use Zend\Db\Adapter\Exception\InvalidQueryException;
@@ -12,35 +12,10 @@ class GroupUserTest extends TableTestCase
 {
     protected function setUp()
     {
-        $configArray   = require($_SERVER['PWD'] . '/config/autoload/local.php');
-        $configArray   = $configArray['db']['adapters']['leogalle_test'];
-        $this->adapter = new Adapter($configArray);
+        $this->dropAndCreateTables(['group', 'group_user']);
 
-        $this->dropTables();
-        $this->createTables();
-
-        $this->groupTable     = new GroupTable\Group($this->adapter);
-        $this->groupUserTable = new GroupTable\GroupUser($this->adapter);
-    }
-
-    protected function dropTables()
-    {
-        $sql = file_get_contents($this->sqlDirectory . '/leogalle_test/group_user/drop.sql');
-        $result = $this->adapter->query($sql)->execute();
-    }
-
-    protected function createTables()
-    {
-        $sql = file_get_contents($this->sqlDirectory . '/leogalle_test/group_user/create.sql');
-        $result = $this->adapter->query($sql)->execute();
-    }
-
-    public function testInitialize()
-    {
-        $this->assertInstanceOf(
-            GroupTable\GroupUser::class,
-            $this->groupUserTable
-        );
+        $this->groupTable     = new GroupTable\Group($this->getAdapter());
+        $this->groupUserTable = new GroupTable\GroupUser($this->getAdapter());
     }
 
     public function testInsertAndSelectCount()
