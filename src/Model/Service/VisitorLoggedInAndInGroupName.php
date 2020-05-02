a4447b50aef1fc66406200a733a76802930005cc
Leo Galleguillos <leo.galleguillos@icloud.com>
2020-05-02 12:50:50 +0000

    Remove unused shortcuts

--- src/Model/Service/VisitorLoggedInAndInGroupName.php
+++ src/Model/Service/VisitorLoggedInAndInGroupName.php
@@ -1,11 +1,8 @@
 <?php
 namespace LeoGalleguillos\Group\Model\Service;
 
-use LeoGalleguillos\Group\Model\Entity as GroupEntity;
 use LeoGalleguillos\Group\Model\Factory as GroupFactory;
 use LeoGalleguillos\Group\Model\Service as GroupService;
-use LeoGalleguillos\Group\Model\Table as GroupTable;
-use LeoGalleguillos\User\Model\Entity as UserEntity;
 use LeoGalleguillos\User\Model\Service as UserService;
 
 class VisitorLoggedInAndInGroupName
